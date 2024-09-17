b51584882965a0ab4335fa1691e728e17d7f1e84
athiprakash <138091956+athiprakash@users.noreply.github.com>
2024-09-17 23:08:29 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+
+int sub(int x, int y){
+if x>y
+  return x-y;
+else
+  return y-x;
+}
+int add(int x, int y){
+
+  return x+y;
+
+}
+
+void main(){
+
+printf("enter two numbers x and y\n");
+scanf("%d%d\t",&x&y);
+add();
+sub();
+}
